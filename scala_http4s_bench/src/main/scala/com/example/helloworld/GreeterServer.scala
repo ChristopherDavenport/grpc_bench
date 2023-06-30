204eae4660cb85a00c4103fa152f441148029ff1
Christopher Davenport <chris.davenport@banno.com>
2023-06-30 09:11:41 -0700

    Accept ports, probably something better here

--- scala_http4s_bench/src/main/scala/com/example/helloworld/GreeterServer.scala
+++ scala_http4s_bench/src/main/scala/com/example/helloworld/GreeterServer.scala
@@ -7,6 +7,7 @@ import org.http4s.implicits._
 import com.comcast.ip4s._
 
 object GreeterServer extends IOApp {
+  val host = host"0.0.0.0"
   val port = port"50051"
   val impl = new GreeterServiceImpl()
   val app = Greeter.toRoutes(impl).orNotFound
@@ -14,6 +15,7 @@ object GreeterServer extends IOApp {
     EmberServerBuilder
       .default[IO]
       .withHttp2
+      .withHost(host)
       .withPort(port)
       .withHttpApp(app)
       .build
