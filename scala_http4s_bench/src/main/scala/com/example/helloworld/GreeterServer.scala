682f6bc8a87f56ebba4d86a5dad7d9d5fa3f8155
Christopher Davenport <chris.davenport@banno.com>
2023-06-30 08:55:15 -0700

    Get correct port

--- scala_http4s_bench/src/main/scala/com/example/helloworld/GreeterServer.scala
+++ scala_http4s_bench/src/main/scala/com/example/helloworld/GreeterServer.scala
@@ -4,14 +4,17 @@ import cats.effect._
 import org.http4s.ember.server.EmberServerBuilder
 import io.grpc.examples.helloworld.helloworld.Greeter
 import org.http4s.implicits._
+import com.comcast.ip4s._
 
 object GreeterServer extends IOApp {
+  val port = port"50051"
   val impl = new GreeterServiceImpl()
   val app = Greeter.toRoutes(impl).orNotFound
   def run(args: List[String]): IO[ExitCode] = {
     EmberServerBuilder
       .default[IO]
       .withHttp2
+      .withPort(port)
       .withHttpApp(app)
       .build
       .useForever
